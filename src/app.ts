84ea9bcea097576fac8586e48211975801d93e7f
Nacho Consolani <nacho.conso@gmail.com>
2021-12-27 10:03:39 -0300

    update gitignore

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import express, { NextFunction, Request, Response } from "express";
 import cors from "cors";
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
-console.log(process.env)
+console.log(process.env);
 const app = express();
 app.use(cors());
 app.use(express.json());
