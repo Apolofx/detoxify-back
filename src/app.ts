f974e3affd89bd2c92f1e97a4e11e72e82c7aa48
Nacho Consolani <nacho.conso@gmail.com>
2021-12-27 22:56:28 -0300

    add healthcheck

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,19 @@ import express, { NextFunction, Request, Response } from "express";
 import cors from "cors";
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
-console.log(process.env);
+
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.get("/health", (_req, res) => {
+  const response = {
+    uptime: process.uptime(),
+    status: "OK",
+    date: new Date(),
+  };
+  res.status(200).json(response);
+});
 
 app.post("/users", async (req, res, next) => {
   const { body: data } = req;
