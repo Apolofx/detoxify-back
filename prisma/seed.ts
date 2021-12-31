32c7b9de5398d2f1b6e97ef912e759ac6d26c11a
Nacho Consolani <nacho.conso@gmail.com>
2021-12-31 01:44:34 -0300

    add seeds

---
+++ prisma/seed.ts
@@ -0,0 +1,25 @@
+import { PrismaClient } from "@prisma/client";
+import faker from "faker";
+const prisma = new PrismaClient();
+
+async function main() {
+  if (process.env.NODE_ENV !== "dev") return;
+  //remove existing records
+  await prisma.user.deleteMany({});
+  //create users with details, events, and achievements
+  console.log("Seeding database");
+  for (let i = 0; i < 100; i++) {
+    await prisma.user.create({
+      data: {
+        name: faker.name.firstName(),
+        email: faker.internet.email(),
+      },
+    });
+  }
+}
+
+main()
+  .catch((e) => console.log(e))
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
