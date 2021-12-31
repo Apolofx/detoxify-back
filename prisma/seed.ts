15f21d26d9947aae47c5b3fe44c87f8199480ab8
Nacho Consolani <nacho.conso@gmail.com>
2021-12-31 13:44:34 -0300

    remove seeding from develop ci workflow

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,11 +3,16 @@ import faker from "faker";
 const prisma = new PrismaClient();
 
 async function main() {
-  if (process.env.NODE_ENV !== "dev") return;
+  if (process.env.NODE_ENV !== "dev") {
+    console.log(
+      `Current environment: ${process.env.NODE_ENV}, no seeds will run ⚠️.`
+    );
+    return;
+  }
   //remove existing records
   await prisma.user.deleteMany({});
   //create users with details, events, and achievements
-  console.log("Seeding database");
+  console.log("Seeding database ✅");
   for (let i = 0; i < 100; i++) {
     await prisma.user.create({
       data: {
