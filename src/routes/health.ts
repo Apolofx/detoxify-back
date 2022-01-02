6c2a677ff1d73e6cc24277714d92c74942e0543d
Nacho Consolani <nacho.conso@gmail.com>
2022-01-02 13:27:28 -0300

    add env to health check

--- src/routes/health.ts
+++ src/routes/health.ts
@@ -7,6 +7,7 @@ health.get("/", (_req, res) => {
     uptime: process.uptime(),
     status: "OK",
     date: new Date(),
+    environment: process.env.NODE_ENV,
   };
   res.status(200).json(response);
 });
