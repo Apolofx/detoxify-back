e52d0fb86c07257f872c86dc235f4883ccbde5a1
Nacho Consolani <nacho.conso@gmail.com>
2022-01-02 12:20:21 -0300

    rearrange routes

---
+++ src/routes/health.ts
@@ -0,0 +1,14 @@
+import express from "express";
+
+const health = express.Router();
+
+health.get("/health", (_req, res) => {
+  const response = {
+    uptime: process.uptime(),
+    status: "OK",
+    date: new Date(),
+  };
+  res.status(200).json(response);
+});
+
+export { health };
