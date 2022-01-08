f45565f7fbae6960954dde4569fda0d6fae7fb5d
Nacho Consolani <nacho.conso@gmail.com>
2022-01-08 14:56:13 -0300

    add open API page on /api-docs endpoint

---
+++ src/docs/index.ts
@@ -0,0 +1,82 @@
+import { createUser, getUser, getUsers } from "./users.swagger";
+export const swaggerDocument = {
+  openapi: "3.0.1",
+  info: {
+    version: "1.0.0",
+    title: "Detoxify API",
+    description: "Detoxify main API docs",
+    termsOfService: "",
+    contact: {
+      name: "Juan Ignacio Consolani",
+      email: "nacho.conso@gmail.com",
+      url: "https://apolodev.ar",
+    },
+    license: {
+      name: "Apache 2.0",
+      url: "https://www.apache.org/licenses/LICENSE-2.0.html",
+    },
+  },
+  servers: [
+    {
+      url: "http://localhost:8080/api",
+      description: "Local server",
+    },
+    {
+      url: "http://dev.detoxify/api",
+      description: "Development/testing environment server",
+    },
+    {
+      url: "http://detoxify/api",
+      description: "Production environment server",
+    },
+  ],
+  tags: [
+    {
+      name: "Users",
+    },
+  ],
+  paths: {
+    "/users": {
+      get: getUsers,
+      post: createUser,
+    },
+    "/users/{userId}": {
+      get: getUser,
+    },
+  },
+  components: {
+    schemas: {
+      ArrayOfUsers: {
+        type: "array",
+        items: {
+          $ref: "#/components/schemas/User",
+        },
+      },
+      User: {
+        type: "object",
+        properties: {
+          id: {
+            type: "integer",
+            format: "int32",
+          },
+          name: {
+            type: "string",
+          },
+          email: {
+            type: "string",
+          },
+          createdAt: {
+            type: "string",
+          },
+          updatedAt: {
+            type: "string",
+          },
+          teamId: {
+            type: "integer",
+            format: "nullable",
+          },
+        },
+      },
+    },
+  },
+};
